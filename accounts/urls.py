67758669715bacbed11180fcf43c5ded700cae7d
David Kwan <dwkwan1989@gmail.com>
2019-09-04 15:37:08 -0700

    Create account and product urls, views, anbd html pages. Define Product class

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name="signup"),
-    path('login/', views.login, name="login"),
-    path('logout/', views.logout, name="logout")
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout')
 ]
 
